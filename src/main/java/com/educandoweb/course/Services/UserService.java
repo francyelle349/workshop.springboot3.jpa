fedbacc1fabddb993888a230cea1b677689bcdec
francyelle349 <francyelle5849@gmail.com>
2025-06-16 20:55:09 -0400

    user delete

--- src/main/java/com/educandoweb/course/Services/UserService.java
+++ src/main/java/com/educandoweb/course/Services/UserService.java
@@ -5,9 +5,13 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+
+import sun.print.resources.serviceui;
 
 @Service
 public class UserService {
@@ -30,4 +34,10 @@ public class UserService {
     public User insert(User obj) {
     	return repository.save(obj);
     }
+    
+    
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+  
 }
